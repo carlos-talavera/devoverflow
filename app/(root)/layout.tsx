7011b4b345d4b7264ef26ad7890106c9f1cbe309
carlos-talavera <carlos.talavera@therocketcode.com>
2024-05-23 16:51:45 -0600

    Add left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import LeftSidebar from "@/components/shared/LeftSidebar"
 import Navbar from "@/components/shared/navbar/Navbar"
 import React from "react"
 
@@ -10,7 +11,7 @@ const Layout = ({ children } : { children: React.ReactNode }) => {
       <div
         className="flex"
       >
-        LeftSidebar
+        <LeftSidebar />
         <section
           className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14"
         >
