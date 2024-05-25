c84f7610d6d3f2a47ee7eb363b35ae127cf9d2ad
carlos-talavera <carlos.talavera@therocketcode.com>
2024-05-24 18:17:37 -0600

    Add home page layout (filters and questions)

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,14 +1,154 @@
-import {
-  SignInButton,
-  SignedOut
-} from "@clerk/nextjs";
+import QuestionCard from "@/components/cards/QuestionCard";
+import HomeFilters from "@/components/home/HomeFilters";
+import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
+import Link from "next/link";
+
+const questions = [
+  {
+    _id: 1,
+    title: "Cascading Deletes in Django like a pro? Give me the best practices!",
+    tags: [
+      {
+        _id: 1,
+        name: "django",
+      },
+      {
+        _id: 2,
+        name: "python",
+      },
+    ],
+    author: {
+      _id: 1,
+      name: "John Doe",
+      picture:
+        "https://randomuser.me/api/portraits/men/1.jpg",
+    },
+    upvotes: Math.floor(Math.random() * 100),
+    views: Math.floor(Math.random() * 1000),
+    answers: Array.from(
+      { length: Math.floor(Math.random() * 10) },
+      (_, index) => ({
+        _id: index,
+        content: "Sample answer content",
+        author: {
+          _id: Math.floor(Math.random() * 100),
+          name: "Random User",
+          picture:
+            "https://randomuser.me/api/portraits/men/2.jpg",
+        },
+        createdAt: new Date().toISOString(),
+      })
+    ),
+    createdAt: new Date(
+      "2021-09-01T00:00:00.000Z"
+    ),
+  },
+  {
+    _id: 2,
+    title: "How to center a div?",
+    tags: [
+      {
+        _id: 3,
+        name: "css",
+      },
+      {
+        _id: 4,
+        name: "html",
+      },
+    ],
+    author: {
+      _id: 2,
+      name: "Jane Doe",
+      picture:
+        "https://randomuser.me/api/portraits/women/2.jpg",
+    },
+    upvotes: Math.floor(Math.random() * 100),
+    views: Math.floor(Math.random() * 1000),
+    answers: Array.from(
+      { length: Math.floor(Math.random() * 10) },
+      (_, index) => ({
+        _id: index,
+        content: "Sample answer content",
+        author: {
+          _id: Math.floor(Math.random() * 100),
+          name: "Random User",
+          picture:
+            "https://randomuser.me/api/portraits/women/3.jpg",
+        },
+        createdAt: new Date().toISOString(),
+      })
+    ),
+    createdAt: new Date(
+      "2021-09-10T00:00:00.000Z"
+    ),
+  },
+];
 
 const Home = () => {
   return (
     <>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">
+          All Questions
+        </h1>
+
+        <Link
+          href="/ask-question"
+          className="flex justify-end max-sm:w-full"
+        >
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
+      </div>
+
+      <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? 
+          questions.map((question) => 
+          (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          )
+        ) : (
+          <NoResult
+            title="There's no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 };
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,43 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getTimestamp = (createdAt: Date): string => {
+  const now = new Date();
+  const diffInSeconds = Math.floor(
+    (now.getTime() - createdAt.getTime()) / 1000
+  );
+
+  const units: [number, string][] = [
+    [60, "second"], // 60 seconds in a minute
+    [60, "minute"], // 60 minutes in an hour
+    [24, "hour"], // 24 hours in a day
+    [30, "day"], // ~30 days in a month
+    [12, "month"], // 12 months in a year
+    [Number.MAX_SAFE_INTEGER, "year"], // years can go indefinitely
+  ];
+
+  let elapsed = diffInSeconds;
+  for (const [unitSize, unitName] of units) {
+    if (elapsed < unitSize) {
+      const roundedElapsed = Math.floor(elapsed);
+      return `${roundedElapsed} ${unitName}${roundedElapsed !== 1 ? "s" : ""} ago`;
+    }
+    elapsed /= unitSize;
+  }
+
+  return "just now"; // fallback in case of an unexpected error
+}
+
+export const formatNumber = (num: number): string => {
+  if (num >= 1_000_000) {
+    return (num / 1_000_000).toFixed(1).replace(/\.0$/, '') + 'M';
+  } else if (num >= 1_000) {
+    return (num / 1_000).toFixed(1).replace(/\.0$/, '') + 'K';
+  } else {
+    return num.toString();
+  }
+};
