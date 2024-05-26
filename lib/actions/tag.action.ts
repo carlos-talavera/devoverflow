cca30d475f90eb7b3bb927bc7023bf127d7e8a65
carlos-talavera <carlos.talavera@therocketcode.com>
2024-05-26 13:20:23 -0600

    add tags page

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,8 +1,9 @@
 "use server";
 
+import Tag from "@/database/tag.model";
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -34,6 +35,21 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
         name: "Next.js"
       }
     ]
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tags = await Tag.find({});
+
+    return {
+      tags
+    }
   } catch (error) {
     console.log(error);
     throw error;
